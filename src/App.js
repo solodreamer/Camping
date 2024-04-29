bcbf9e7ef6b7dcb0fc33dc280bd437ad4f4b4a9b
郭政芃 <a33760@mail.cmuh.org.tw>
2024-04-29 17:36:21 +0800

    更新登入頁面的按鈕和錯誤訊息

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { useEffect } from "react";
-import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import { useState } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-// import { Button, ButtonGroup } from '@chakra-ui/react';
-// import React from 'react';
-import { Button } from "antd";
 
 function App() {
   // useEffect(() => {
@@ -41,90 +34,12 @@ function App() {
   //   }
   // }
 
-  const [restostring, setRestostring] = useState("尚未登入");
-
-  const [data, setData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setData({ ...data, [name]: value });
-    // console.log(name, value);
-    // console.log(data);
-  };
-
-  const submit = async (e) => {
-    try {
-      const formData = new FormData();
-      formData.append("username", data.username);
-      formData.append("password", data.password);
-      const res = await axios.post(`http://172.105.209.194/v1/login`, formData);
-      console.log(res);
-      console.log(restostring);
-      const { token } = res.data;
-      setRestostring(token);
-      // axios.defaults.headers.common['Authorization'] = token;
-      // const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products/all`);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const clear = (e) => {
-    setRestostring("尚未登入");
-  };
-
   return (
-    <ChakraProvider>
-      <div className="App">
-        {/* <Routes>
-        <Route path='/login' element={<Login/>}></Route>
-      </Routes> */}
-        <div className="container py-5">
-          <div className="row justify-content-center">
-            <div className="col-md-6">
-              <h2>登入帳號</h2>
-
-              <div className="alert alert-danger" role="alert">
-                {restostring}
-              </div>
-              <div className="mb-2">
-                <label htmlFor="email" className="form-label w-100">
-                  Email
-                  <input
-                    id="email"
-                    className="form-control"
-                    name="username"
-                    type="email"
-                    placeholder="Email Address"
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="mb-2">
-                <label htmlFor="password" className="form-label w-100">
-                  密碼
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    id="password"
-                    placeholder="name@example.com"
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <Button colorScheme="teal" mr="5px" onClick={submit}>
-                登入
-              </Button>
-              <Button onClick={clear}>清空</Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </ChakraProvider>
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,55 +1,88 @@
 import axios from "axios";
 import { useState } from "react";
+import { Button } from "antd";
 
 function Login() {
-    const [data, setData] = useState({
-        username: '',
-        password: ''
-    }); //初始化data為空物件
+  const [restostring, setRestostring] = useState("尚未登入");
 
-    const handleChange = (e) => {
-        const {name, value}  = e.target;
-        setData({...data, [name]: value});
-        // console.log(name, value);
-        // console.log(data);
-    }
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const submit = async (e) => {
-        const res = await axios.post(`/v2/admin/signin`, data);
-        // console.log(res);
-        const {token} = res.data;
-        axios.defaults.headers.common['Authorization'] = token;
-        const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products/all`);
-        // const productRes = await axios.get(`http://172.105.209.194/api/test`);
-        console.log(productRes);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+    // console.log(name, value);
+    // console.log(data);
+  };
+
+  const login = async (e) => {
+    try {
+      const formData = new FormData();
+      formData.append("username", data.username);
+      formData.append("password", data.password);
+      const res = await axios.post(`http://172.105.209.194/v1/login`, formData);
+      console.log(res);
+      console.log(restostring);
+      const { token } = res.data;
+      setRestostring(token);
+      // axios.defaults.headers.common['Authorization'] = token;
+      // const productRes = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products/all`);
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  const clear = (e) => {
+    setRestostring("尚未登入");
+  };
 
   return (
-  <div className="container py-5">
-    <div className="row justify-content-center">
-      <div className="col-md-6">
-        <h2>登入帳號</h2>
-
-        <div className="alert alert-danger" role="alert">
-          錯誤訊息
+    <div className="App">
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <h2>登入帳號</h2>
+            <div className="alert alert-danger" role="alert">
+              {restostring}
+            </div>
+            <div className="mb-2">
+              <label htmlFor="email" className="form-label w-100">
+                Email
+                <input
+                  id="email"
+                  className="form-control"
+                  name="username"
+                  type="email"
+                  placeholder="Email Address"
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="mb-2">
+              <label htmlFor="password" className="form-label w-100">
+                密碼
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  id="password"
+                  placeholder="name@example.com"
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <Button onClick={clear}>清空</Button>
+            <Button>註冊</Button>
+            <Button type="primary" onClick={login}>
+              登入
+            </Button>
+          </div>
         </div>
-        <div className="mb-2">
-          <label htmlFor="email" className="form-label w-100">
-            Email
-            <input id="email" className="form-control" name="username" type="email" placeholder="Email Address"  onChange={handleChange}/>
-          </label>
-        </div>
-        <div className="mb-2">
-          <label htmlFor="password" className="form-label w-100">
-            密碼
-            <input type="password" className="form-control"  name="password" id="password" placeholder="name@example.com"  onChange={handleChange} />
-          </label>
-        </div>
-        <button type="button" className="btn btn-primary" onClick={submit}>登入</button>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Login;
