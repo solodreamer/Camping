89426b7f2552bdff4d1c8c0e40f7a470bf78302a
Solodreamer <k95175g@gmail.com>
2024-05-23 00:52:39 +0800

    chore: Update Home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  AutoComplete,
+  Button,
+  Cascader,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Select,
+  DatePicker,
+  Space,
+} from "antd";
+
+
+function Home() {
+
+    return (
+        <div className="home">
+            <div className="header">
+            <h2>首頁</h2>
+            </div>
+            <div className="body"></div>
+            <div className="footer"></div>
+        </div>
+    )
+
+}
+
+export default Home;
