58efe882dda4c85b5ab8ffb91b3215f1b3b6113e
郭政芃 <a33760@mail.cmuh.org.tw>
2024-06-25 18:03:01 +0800

    feat():註冊頁面新增grid響應式排版

--- src/pages/register.js
+++ src/pages/register.js
@@ -89,7 +89,6 @@ function Register() {
     // form.setFieldsValue({ birthday: dateString });
   };
 
-
   const handleChange = (e) => {
     const { value } = e.target;
     setPhone({ phone: value });
@@ -99,14 +98,14 @@ function Register() {
   useEffect(() => {
     let timerId;
     if (countdown > 0) {
-      timerId = setInterval(()=> {
+      timerId = setInterval(() => {
         setCountdown(countdown - 1);
-        console.log('countdown',countdown);
+        console.log("countdown", countdown);
       }, 1000);
-      console.log('timerId1',timerId);
+      console.log("timerId1", timerId);
     }
     return () => {
-      console.log('timerId2',timerId);
+      console.log("timerId2", timerId);
       if (timerId) {
         clearInterval(timerId);
       }
@@ -116,13 +115,16 @@ function Register() {
   /** 取得手機驗證碼api */
   const getVerifyCode = async () => {
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/v1/auth/signup/send-verify-code`, phone);
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/v1/auth/signup/send-verify-code`,
+        phone
+      );
       console.log("getVerifyCodeApi:", res);
-      if(res.data.success === true) {
+      if (res.data.success === true) {
         setCountdown(30);
       }
     } catch (err) {
-      console.log("getVerifyCodeApiErr:",err);
+      console.log("getVerifyCodeApiErr:", err);
     }
   };
 
@@ -156,186 +158,204 @@ function Register() {
     <Layout style={layoutStyle}>
       <Header style={headerStyle}>Header</Header>
       <Content style={contentStyle}>
-      <img
-        src='https://images.unsplash.com/photo-1470246973918-29a93221c455?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhbXB8ZW58MHx8MHx8fDA%3D'
-        sizes="(min-width: 1335px) 410.6666666666667px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
-        className='object-cover'
-      ></img>
-        <Form
-          {...formItemLayout}
-          form={form}
-          name="register"
-          onFinish={onFinish}
-          initialValues={{
-            residence: ["zhejiang", "hangzhou", "xihu"],
-            prefix: "86",
-          }}
-          style={{
-            maxWidth: 600,
-          }}
-          scrollToFirstError
-        >
-          <Form.Item
-            name="name"
-            label="姓名"
-            rules={[
-              {
-                required: true,
-                message: "請輸入最少兩個字",
-                whitespace: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="sex"
-            label="性別"
-            rules={[
-              {
-                required: true,
-                message: "請選擇性別",
-              },
-            ]}
-          >
-            <Select placeholder="select your gender">
-              <Option value="m">男性</Option>
-              <Option value="f">女性</Option>
-              <Option value="o">其他</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name="birthday"
-            label="出生年月日"
-            rules={[{ required: true, message: "請選擇生日" }]}
-          >
-            <DatePicker onChange={dateTimeOnChange} />
-          </Form.Item>
-          <Form.Item
-            name="phone"
-            label="手機號碼"
-            rules={[
-              {
-                required: true,
-                message: "請輸入手機號碼",
-              },
-            ]}
-          >
-            <Input
-              style={{
-                width: "100%",
-              }}
-              onChange={handleChange}
-              value={phone}
-            />
-          </Form.Item>
-          <Form.Item label="驗證碼">
-            <Row gutter={8}>
-              <Col span={12}>
+        <Row>
+          <Col md={0} lg={8}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1470246973918-29a93221c455?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhbXB8ZW58MHx8MHx8fDA%3D"
+                sizes="(min-width: 1335px) 410.6666666666667px, (min-width: 992px) calc(calc(100vw - 88px) / 3), (min-width: 768px) calc(calc(100vw - 64px) / 2), 100vw"
+                className="object-cover"
+              ></img>
+            </div>
+          </Col>
+          <Col md={24} lg={16}>
+            <div>
+              <Form
+                {...formItemLayout}
+                form={form}
+                name="register"
+                onFinish={onFinish}
+                initialValues={{
+                  residence: ["zhejiang", "hangzhou", "xihu"],
+                  prefix: "86",
+                }}
+                style={{
+                  maxWidth: 600,
+                }}
+                scrollToFirstError
+              >
                 <Form.Item
-                  name="verify_code"
-                  noStyle
-                  rules={[{ required: true, message: "請輸入驗證碼" }]}
+                  name="name"
+                  label="姓名"
+                  rules={[
+                    {
+                      required: true,
+                      message: "請輸入最少兩個字",
+                      whitespace: true,
+                    },
+                  ]}
                 >
                   <Input />
                 </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Button type="primary" onClick={getVerifyCode} disabled={countdown > 0}>
-                  { countdown > 0 ? `${countdown}秒後重新獲取` : '取得驗證碼'}
-                </Button>
-              </Col>
-            </Row>
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="電子郵件"
-            rules={[
-              {
-                type: "email",
-                message: "這不是有效的電子郵件",
-              },
-              {
-                required: true,
-                message: "請輸入電子郵件",
-              },
-            ]}
-          >
-            <Input placeholder="name@example.com" />
-          </Form.Item>
+                <Form.Item
+                  name="sex"
+                  label="性別"
+                  rules={[
+                    {
+                      required: true,
+                      message: "請選擇性別",
+                    },
+                  ]}
+                >
+                  <Select placeholder="select your gender">
+                    <Option value="m">男性</Option>
+                    <Option value="f">女性</Option>
+                    <Option value="o">其他</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item
+                  name="birthday"
+                  label="出生年月日"
+                  rules={[{ required: true, message: "請選擇生日" }]}
+                >
+                  <DatePicker onChange={dateTimeOnChange} />
+                </Form.Item>
+                <Form.Item
+                  name="phone"
+                  label="手機號碼"
+                  rules={[
+                    {
+                      required: true,
+                      message: "請輸入手機號碼",
+                    },
+                  ]}
+                >
+                  <Input
+                    style={{
+                      width: "100%",
+                    }}
+                    onChange={handleChange}
+                    value={phone}
+                  />
+                </Form.Item>
+                <Form.Item label="驗證碼">
+                  <Row gutter={8}>
+                    <Col span={12}>
+                      <Form.Item
+                        name="verify_code"
+                        noStyle
+                        rules={[{ required: true, message: "請輸入驗證碼" }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                      <Button
+                        type="primary"
+                        onClick={getVerifyCode}
+                        disabled={countdown > 0}
+                      >
+                        {countdown > 0
+                          ? `${countdown}秒後重新獲取`
+                          : "取得驗證碼"}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  label="電子郵件"
+                  rules={[
+                    {
+                      type: "email",
+                      message: "這不是有效的電子郵件",
+                    },
+                    {
+                      required: true,
+                      message: "請輸入電子郵件",
+                    },
+                  ]}
+                >
+                  <Input placeholder="name@example.com" />
+                </Form.Item>
 
-          <Form.Item
-            name="password"
-            label="密碼"
-            rules={[
-              {
-                required: true,
-                message: "請輸入密碼",
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password placeholder="建議混合8個字以上的英文字母、數字和符號" />
-          </Form.Item>
+                <Form.Item
+                  name="password"
+                  label="密碼"
+                  rules={[
+                    {
+                      required: true,
+                      message: "請輸入密碼",
+                    },
+                  ]}
+                  hasFeedback
+                >
+                  <Input.Password placeholder="建議混合8個字以上的英文字母、數字和符號" />
+                </Form.Item>
 
-          <Form.Item
-            name="password_confirm"
-            label="再次確認密碼"
-            dependencies={["password"]}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: "請再次輸入密碼",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(new Error("密碼不相符!"));
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            name="agreement"
-            valuePropName="checked"
-            rules={[
-              {
-                validator: (_, value) =>
-                  value
-                    ? Promise.resolve()
-                    : Promise.reject(new Error("Should accept agreement")),
-              },
-            ]}
-            {...tailFormItemLayout}
-          >
-            <Checkbox>
-              我已詳細閱讀並同意{" "}
-              <NavLink
-                to="/privacyPolicy"
-                style={{ color: "blue", textDecoration: "underline" }}
-              >
-                隱私政策
-              </NavLink>
-              、
-              <NavLink
-                to="/termService"
-                style={{ color: "blue", textDecoration: "underline" }}
-              >
-                服務條款
-              </NavLink>
-            </Checkbox>
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit" size="large">
-              建立帳號
-            </Button>
-          </Form.Item>
-        </Form>
+                <Form.Item
+                  name="password_confirm"
+                  label="再次確認密碼"
+                  dependencies={["password"]}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: "請再次輸入密碼",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue("password") === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(new Error("密碼不相符!"));
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+                <Form.Item
+                  name="agreement"
+                  valuePropName="checked"
+                  rules={[
+                    {
+                      validator: (_, value) =>
+                        value
+                          ? Promise.resolve()
+                          : Promise.reject(
+                              new Error("Should accept agreement")
+                            ),
+                    },
+                  ]}
+                  {...tailFormItemLayout}
+                >
+                  <Checkbox>
+                    我已詳細閱讀並同意{" "}
+                    <NavLink
+                      to="/privacyPolicy"
+                      style={{ color: "blue", textDecoration: "underline" }}
+                    >
+                      隱私政策
+                    </NavLink>
+                    、
+                    <NavLink
+                      to="/termService"
+                      style={{ color: "blue", textDecoration: "underline" }}
+                    >
+                      服務條款
+                    </NavLink>
+                  </Checkbox>
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                  <Button type="primary" htmlType="submit" size="large">
+                    建立帳號
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </Col>
+        </Row>
       </Content>
       <Footer style={footerStyle}>Footer</Footer>
     </Layout>
