53a7c7f21d093a4bab91ef58646d64de950a28f2
Solodreamer <k95175g@gmail.com>
2024-08-28 01:30:01 +0800

    feat: 更新詳細營區頁面

--- src/App.js
+++ src/App.js
@@ -10,35 +10,6 @@ import SearchPage from "./pages/searchPage";
 import DetailCamp from "./pages/detailCamp";
 
 function App() {
-  // useEffect(() => {
-  //     console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_API_PATH);
-  //     (async () => {
-  //         const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`);
-  //         console.log('我是axios', res);
-  //       })()
-  //     }, [])
-
-  // useEffect(() => {
-  //   // fetch("https://student-json-api.lidemy.me/comments?_sort=createdAt&_order=desc")
-  //   // fetch("https://ec-course-api.hexschool.io/v2/api/john-test/products/all")
-  //   fetch("http://172.105.209.194/api/test", { method: "GET" })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log('我是fetch', data);
-  //     })
-  //     .catch((err) => {
-  //       console.log('我是fetch', err);
-  //     })
-  // }, [])
-
-  // const submit = async(e) => {
-  //   try {
-  //     const res = await axios.get(`http://172.105.209.194/api/test`); 
-  //     console.log('我是fetch', res);
-  //   } catch (err) {
-
-  //   }
-  // }
 
   return (
     <div className="App">
--- src/pages/detailCamp.js
+++ src/pages/detailCamp.js
@@ -1,16 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
 
-import dayjs from "dayjs";
-import axios from "axios";
-
-import { Layout, Menu, theme, Col, Row, Divider, Typography,} from "antd";
-import { LoginOutlined, UserAddOutlined, HomeOutlined,} from "@ant-design/icons";
-import { QueryFilter, ProFormSelect, ProFormText, ProFormDateRangePicker} from '@ant-design/pro-components';
+import { Layout, Menu, theme, Col, Row, Divider, Typography, } from "antd";
+import { LoginOutlined, UserAddOutlined, HomeOutlined, } from "@ant-design/icons";
 
 //Antd元件屬性設定
 const { Header, Content, Footer, Sider } = Layout;
-const { Title } = Typography;
+const { Title, Paragraph, Text } = Typography;
+const blockContent = {
+  "data": {
+    "campPhotos": [
+      {
+        "img": "https://fakeimg.pl/350x350/?text=Hello"
+      },
+      {
+        "img": "https://fakeimg.pl/350x350/?text=abs"
+      }
+    ],
+    "campsite": [
+      {
+        "areaName": "A區",
+        "campId": 1,
+        "holidayPrice": 1500,
+        "surfaceType": 1,
+        "weekdayPrice": 800
+      },
+      {
+        "areaName": "B區",
+        "campId": 1,
+        "holidayPrice": 1200,
+        "surfaceType": 2,
+        "weekdayPrice": 700
+      }
+    ],
+    "coverImage": "mountain.jpg",
+    "desc": "Explore the mountains",
+    "fullAddress": "台中市南區福興街600號",
+    "id": 1,
+    "name": "Mountain Adventure Camp",
+    "ownerId": 1
+  },
+  "success": true
+};
 
 // 日期格式
 const dateFormat = "YYYY-MM-DD";
@@ -48,71 +79,11 @@ const menuItems = [
   { key: "3", label: "首頁", icon: <HomeOutlined />, path: "/" },
 ];
 
-//地區選單
-const locations = [
-  { value: 1, label: "北部" },
-  { value: 2, label: "中部" },
-  { value: 3, label: "南部" },
-  { value: 4, label: "東部" },
-  { value: 5, label: "離島" },
-];
 
 
 function DetailCamp() {
 
-  /** 未釐清變數 */
-  const navigate = useNavigate();
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-  /** 營地清單設定 */
-  const [camps, setCamps] = useState([]);
 
-  /** 初始化取得營地清單 */
-  useEffect(() => {
-    getCampList();
-  }, []);
-
-  /** 營區清單log */
-  useEffect(() => {
-    console.log("getCampList 更新了:", camps);
-  }, [camps]);
-
-  /** 取得營地清單 */
-  const getCampList = async () => {
-    try {
-      const res = await axios.get(`${process.env.REACT_APP_API_URL}/v1/camps`);
-      if (res.data.success === true) {
-        setCamps(res.data.data);
-      }
-    } catch (err) {
-      console.log("getCampList取得異常:", err);
-    }
-  };
-
-  /** 特定條件查詢營區 */
-  const onSearch = async (values) => {
-    await waitTime(1000);
-    const queryString = new URLSearchParams(values).toString();
-    console.log("homepage準備要傳的資料",queryString);
-    navigate(`/searchPage?${queryString}`);
-  };
-
-  /** 等待時間 */
-  const waitTime = (time) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(true);
-      }, time);
-    });
-  };
-
-  /** 是否禁用日期 */
-  const disabledDate = (current) => {
-    return current && current < dayjs().endOf("day");
-  }
-  
-  
   return (
     <Layout>
       <Sider style={siderStyle} breakpoint="md" collapsedWidth="0">
@@ -127,57 +98,52 @@ function DetailCamp() {
       <Layout>
         <Header style={headerStyle}>Go露營</Header>
         <Content style={contentStyle}>
-          <div>
-             <QueryFilter defaultCollapsed submitter onFinish={onSearch}>
-                <ProFormSelect name="region" label="選擇地區" options={locations}  placeholder="請選擇地區"/>
-                <ProFormDateRangePicker name="dateRange" label="日期" initialValue={[dayjs(), dayjs()]} fieldProps={{disabledDate, inputReadOnly: true}} />
-                <ProFormText name="name" label="關鍵字" placeholder="搜索..."/>
-             </QueryFilter>
-          </div>
           <Typography>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 800, 
-                background: colorBgContainer,
-                borderRadius: borderRadiusLG,
-              }}
-            >
-              <Divider orientation="left" orientationMargin="0">
-                <Title level={2}>熱門營地</Title>
-              </Divider>
-              <Row
-                gutter={{
-                  xs: 8,
-                  sm: 16,
-                  md: 24,
-                  lg: 32,
-                }}
-              >
-                {camps?.map((camp) => {
-                  return (
-                    <Col key={camp.id} xs={24} sm={12} md={8} lg={6} xl={6}>
-                      .
-                      <div key={camp.id}>
-                        <img
-                          src={camp.coverImage}
-                          alt="營區圖片"
-                          className="card-img-top rounded-0 object-cover"
-                          height={300}
-                        />
-                        <h4 className="mb-0 mt-2">{camp.name}</h4>
-                        <Link
-                          // to={`/product/${product.id}`}
-                          className="btn btn-outline-dark rounded-0 text-nowrap mt-2"
-                        >
-                          詳細資訊
-                        </Link>
-                      </div>
-                    </Col>
-                  );
-                })}
-              </Row>
-            </div>
+            <Title>{blockContent.data.name}</Title>
+
+            <Paragraph>
+              {blockContent.data.desc}
+            </Paragraph>
+
+            <Paragraph>
+              <Text strong underline>
+              <NavLink to="https://www.google.com/maps">
+              {blockContent.data.fullAddress}
+              </NavLink>
+              </Text>
+            </Paragraph>
+
+            <Divider />
+
+            <Title level={2}>Guidelines and Resources</Title>
+
+            <Paragraph>
+              We supply a series of design principles, practical patterns and high quality design resources
+              (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
+              prototypes beautifully and efficiently.
+            </Paragraph>
+
+            <Paragraph>
+              <ul>
+                <li>
+                  <Link href="/docs/spec/proximity">Principles</Link>
+                </li>
+                <li>
+                  <Link href="/docs/spec/overview">Patterns</Link>
+                </li>
+                <li>
+                  <Link href="/docs/resources">Resource Download</Link>
+                </li>
+              </ul>
+            </Paragraph>
+
+            <Paragraph>
+              Press <Text keyboard>Esc</Text> to exit...
+            </Paragraph>
+
+            <Divider />
+
+
           </Typography>
         </Content>
         <Footer style={footerStyle}>
