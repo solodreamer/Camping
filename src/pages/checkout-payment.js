606584c01fa3028c4d911a486ccc575bb1298c58
Solodreamer <k95175g@gmail.com>
2025-03-26 18:55:53 +0800

    feat(): 更新結帳付款頁面，新增持卡人姓名、卡號、背面末三碼及有效期限欄位

--- src/pages/checkout-payment.js
+++ src/pages/checkout-payment.js
@@ -94,11 +94,12 @@ function CheckoutPayment() {
       <Layout>
         <Header className="checkoutConfirm-headerStyle">Go露營</Header>
         <Content className="checkoutConfirm-contentStyle">
-          <StepsConfirm currentStep={1}/>
+          <StepsConfirm currentStep={1} />
           <Row className="contentRow" >
             <Col className="contentCol1" xs={24} sm={24} md={15} lg={15} xl={15}>
-              <Form {...formItemLayout} name="confirmForm" layout="vertical" size="Large"> 
-                <Form.Item label="訂購人姓名" name="name" rules={[
+              <Form {...formItemLayout} name="paymentForm" layout="vertical" size="Large"
+                  initialValues={{ cardHolderName: detail1?.name }}>
+                <Form.Item label="持卡人姓名" name="cardHolderName" rules={[
                   {
                     required: true,
                     min: 2,
@@ -109,33 +110,98 @@ function CheckoutPayment() {
                     max: 20,
                     message: "已達最大字數!",
                   },
-                    ]}
-                  style={{ marginBottom: 40}}
-                  > 
-                  <Input /> 
+                ]}
+                  style={{ marginBottom: 40 }}
+                >
+                  <Input placeholder="請輸入持卡人姓名" />
                 </Form.Item>
-                <Form.Item label="手機號碼" name="phone" rules={[
+                <Form.Item label="卡號" name="cardNumber" rules={[
                   {
                     required: true,
-                    message: "請輸入手機號碼",
+                    message: "請輸入卡號",
                   },
                   {
-                    len: 10,
-                    message: "請輸入10碼手機號碼",
+                    len: 16,
+                    message: "卡號必須為 16 位數字",
                   }
-                    ]}
-                  style={{ marginBottom: 40}}
-                  > 
-                  <Input /> 
+                ]}
+                  style={{ marginBottom: 40 }}
+                >
+                  <Input placeholder="請輸入卡號" maxLength={16} />
                 </Form.Item>
-                <Form.Item label="備註" name="remark"> <TextArea  showCount maxLength={255} /> </Form.Item>
+                <Form.Item label="背面末三碼" name="cvv" rules={[
+                  {
+                    required: true,
+                    message: "請輸入背面末三碼",
+                  },
+                  {
+                    len: 3,
+                    message: "背面末三碼必須為 3 位數字",
+                  },
+                  {
+                    pattern: /^\d+$/,
+                    message: "背面末三碼只能包含數字",
+                  },
+                ]}
+                  style={{ marginBottom: 40 }}>
+                  <Input placeholder="請輸入背面末三碼" maxLength={3} />
+                </Form.Item>
+                <Form.Item label="有效期限" required style={{ marginBottom: 40 }}>
+                  <Input.Group compact>
+                    <Form.Item
+                      name="expiryMonth"
+                      noStyle
+                      rules={[
+                        {
+                          required: true,
+                          message: "請選擇月份",
+                        },
+                      ]}
+                    >
+                      <Select placeholder="月" style={{ width: "50%" }}>
+                        {Array.from({ length: 12 }, (_, i) => (
+                          <Select.Option key={i + 1} value={String(i + 1).padStart(2, "0")}>
+                            {String(i + 1).padStart(2, "0")}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                    <Form.Item
+                      name="expiryYear"
+                      noStyle
+                      rules={[
+                        {
+                          required: true,
+                          message: "請選擇年份",
+                        },
+                      ]}
+                    >
+                      <Select placeholder="年" style={{ width: "50%" }}>
+                        {Array.from({ length: 10 }, (_, i) => {
+                          const year = new Date().getFullYear() + i;
+                          const yearShort = String(year).slice(-2); // 取西元年的後兩碼
+                          return (
+                            <Select.Option key={year} value={String(yearShort)}>
+                              {yearShort}
+                            </Select.Option>
+                          );
+                        })}
+                      </Select>
+                    </Form.Item>
+                  </Input.Group>
+                </Form.Item>
+                <Form.Item wrapperCol={{span: 15, offset: 7}}>
+                  <Button type="primary" htmlType="submit" size="large">
+                    確認付款
+                  </Button>
+                </Form.Item>
               </Form>
             </Col>
             <Col className="contentCol2" xs={24} sm={24} md={9} lg={9} xl={9}>
               <Divider orientation="left" orientationMargin="0">
                 <Title level={5}>明細確認</Title>
               </Divider>
-              <CheckDetail result={bookingResult}/>
+              <CheckDetail result={bookingResult} />
               <div className="check-total">
                 <div className="checktt-list">
                   <p className='checktt-list checktt-total h4'>總計</p>
